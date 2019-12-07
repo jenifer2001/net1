49ea1ffeb7742e6c84ec94166515caf97fd56af8
UPN <UPN@ULNLAB1408>
2019-12-07 11:59:53 -0500

    imprimiendo mensaje de año nuevo

---
+++ JavaApplication73/src/javaapplication73/JavaApplication73.java
@@ -0,0 +1,14 @@
+
+package javaapplication73;
+
+
+public class JavaApplication73 {
+
+    
+    public static void main(String[] args) {
+    
+        System.out.println("Feliz año 2020");
+        
+    }
+    
+}
